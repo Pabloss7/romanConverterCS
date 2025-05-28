796e060b5dee909f9669959422eb5e6efc2f76a9
Pabloss7 <30286013+Pabloss7@users.noreply.github.com>
2025-05-28 18:59:00 +0200

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+moule.exports = [
+  {
+    "rules": {
+        "no-cond-assign":[
+          "error",
+          "always"
+        ],
+        "indent": [
+          "error",
+          2
+        ],
+        "linebreak-style": [
+          "error",
+          "unix"
+        ],
+        "quotes": [
+          "error",
+          "single
+        ],
+        "no-unused-vars": "off",
+        "semi": [
+          "error",
+          "always"
+        ]
+      }
+  } 
+];
